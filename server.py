8c4b94170c1e0f94f15841759d5bd44baa7dfbe5
Hackbright Student <info@hackbrightacademy.com>
2016-05-03 18:00:43 -0700

    incorporating flash

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
+
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,7 +23,45 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users"""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/login_page")
+def sign_in_form():
+    """Show login page"""
+
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    """Log in user or create new user"""
+
+    #check to see if the user is in db
+    email = request.form.get("email")
+    password = request.form.get("pwd")
+
+    #if yes log them in. 
+    #Checking to see if the email is already in the database
+    user = Users.query.filter_by(email=email).all()
+    
+    if user:
+        #check if password matched
+        user.password == password
+        #yes, flash you are log in
+        #no, the password didnt match try again
+        #re-route them to the sign-in page
+    else:
+        # add new user to db
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        #log them in
 
 
 if __name__ == "__main__":
