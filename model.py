dc9c16237d88b8dbb7256bea56bfc0845944ea7c
Hackbright Student <info@hackbrightacademy.com>
2016-05-05 18:02:33 -0700

    finished step 3 part 3

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models and database functions for Ratings project."""
 
 from flask_sqlalchemy import SQLAlchemy
+import correlation
 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -30,6 +31,26 @@ class User(db.Model):
                                                                 self.email, 
                                                                 self.age, 
                                                                 self.zipcode)
+
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.score, r.score) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+
+        else:
+            return 0.0
 
 
 # Put your Movie and Rating model classes here.
@@ -74,6 +95,8 @@ class Rating(db.Model):
 
 ##############################################################################
 # Helper functions
+
+
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
